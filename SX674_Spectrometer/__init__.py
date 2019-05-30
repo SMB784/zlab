b6e1554ebc3030557647613783bdbe2f0b8e383a
SMB784 <s.blakley86@gmail.com>
2019-05-30 18:57:21 -0500

    Initial commit

---
+++ SX674_Spectrometer/__init__.py
@@ -0,0 +1,8 @@
+from astropy.io import fits
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+from scipy import stats
+from lmfit import Model
+import os,re
