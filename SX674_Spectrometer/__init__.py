0f9fd40a3796b33e04eb23f32299a9fae6d64ac5
SMB784 <s.blakley86@gmail.com>
2019-05-30 23:04:56 -0500

    Moved numerical_sort and find_max_window functions to init

--- SX674_Spectrometer/__init__.py
+++ SX674_Spectrometer/__init__.py
@@ -6,3 +6,30 @@ from pathlib import Path
 from scipy import stats
 from lmfit import Model
 import os,re
+
+baseline=800.0 # No Binning
+# baseline=1100.0 # 4x4 Binning
+trigger=1.2
+gate=10
+
+numbers=re.compile(r'(\d+)')
+
+def numerical_sort(value):
+    parts=numbers.split(value)
+    parts[1::2]=map(int,parts[1::2])
+    return parts
+
+def find_max_window(df):
+    rollingArray=df[0:len(df[0])].rolling(window=gate).mean()[gate-1:len(df[0])]
+    loc=[]
+    y_window=[]
+    for i in range(0,len(df.columns)):
+        vals=rollingArray[i].values
+        try:
+            y=np.min(np.argwhere(vals>=baseline*trigger))
+            y_window.append(np.min(np.argwhere(vals[y:len(vals)]<=baseline*trigger)))
+        except:
+            y=0
+            y_window.append(0)
+        loc.append(y)
+    return [np.max(y_window),loc]
--- SX674_Spectrometer/spectrum_read.py
+++ SX674_Spectrometer/spectrum_read.py
@@ -1,95 +1,24 @@
 from SX674_Spectrometer import *
+from SX674_Spectrometer import numerical_sort, find_max_window
 
 data_root_directory='/home/sean/Desktop/5-29-19/NewSpectrometer/'
 # sub_folder='27000uW_10ms_4x4Binning/'
 sub_folder='27000uW_250ms_NoBin/'
 save_folder='spectralData/'
-plt.figure(figsize=(10,8))
 
-baseline=800.0 # No Binning
-# baseline=1100.0 # 4x4 Binning
-trigger=1.2
-gate=10
 # calibration=[543.26,0.13497] #4x4 binning
 calibration=[543.741,0.068256] #No binning
 
-# start=595
-# stop=620.0
 start=595
 stop=630
 tolerance=0.2
 GeV=601
 
-numbers=re.compile(r'(\d+)')
-
-def tripleLorentzianFit(x,c1,c2,c3,\
-                w1,w2,w3,\
-                h1,h2,h3,\
-                o1):
-    return h1/(np.pi*w1)*(w1**2/((x-c1)**2+w1**2))\
-           +h2/(np.pi*w2)*(w2**2/((x-c2)**2+w2**2))\
-           +h3/(np.pi*w3)*(w3**2/((x-c3)**2+w3**2))\
-           +o1
-           
-def doubleLorentzianFit(x,c1,c2,\
-                w1,w2,\
-                h1,h2,\
-                o1):
-    return h1/(np.pi*w1)*(w1**2/((x-c1)**2+w1**2))\
-           +h2/(np.pi*w2)*(w2**2/((x-c2)**2+w2**2))\
-           +o1
-
-def findValue(bestValues): #input is dictionary output from result.best_values
-    center_FWHM=[]
-    for key, value in bestValues.items():
-        if np.isclose(a=value,b=GeV,atol=tolerance*20):
-                center_FWHM.append(value) # center wavelength
-                widthKey='w'+re.findall(r'\d+',key)[0]
-                center_FWHM.append(2*bestValues[widthKey]) # width
-    return center_FWHM
-
-def numericalSort(value):
-    parts=numbers.split(value)
-    parts[1::2]=map(int,parts[1::2])
-    return parts
-pd.options.display.max_rows=1451
-np.set_printoptions(threshold=np.inf)
-
-def find_max_window(df):
-    rollingArray=df[0:len(df[0])].rolling(window=gate).mean()[gate-1:len(df[0])]
-    loc=[]
-    y_window=[]
-    for i in range(0,len(df.columns)):
-        vals=rollingArray[i].values
-#         try:
-#             y_min=np.min(np.argwhere(vals>=baseline*trigger))
-#             y_max=y_min+np.min(np.argwhere(vals[y_min:len(vals)]<=baseline*trigger))
-#             y_window.append(y_max-y_min)
-# 
-#         except:
-#             y_min=0
-#             y_window.append(0)
-# 
-#         loc.append(y_min)
-#     return[y_window,loc]
-        try:
-            y=np.min(np.argwhere(vals>=baseline*trigger))
-            y_window.append(np.min(np.argwhere(vals[y:len(vals)]<=baseline*trigger)))
-        except:
-            y=0
-            y_window.append(0)
-        loc.append(y)
- 
-    return [np.max(y_window),loc]
-
-
-
-#######################################################################################################
 for root,dirs,files in os.walk(Path(data_root_directory+sub_folder)):
-    dirs.sort(key=numericalSort) # sorts directories by ascending power
+    dirs.sort(key=numerical_sort) # sorts directories by ascending power
     
-    for file in sorted(files,key=numericalSort): 
-        files.sort(key=numericalSort)
+    for file in sorted(files,key=numerical_sort): 
+        files.sort(key=numerical_sort)
 
         hdul = fits.open(Path(data_root_directory+sub_folder)/file)
         #reads in data from file, converts to float type, drops first two and last columns (keep 2->len-1)
@@ -119,6 +48,6 @@ for root,dirs,files in os.walk(Path(data_root_directory+sub_folder)):
 
 #         plt.plot(wavelength,normalized_amplitude)
 #         plt.plot(wavelength,result.best_fit,ls='dashed')
-        spectrumData=pd.DataFrame(np.transpose([wavelength,amplitude]))
+        spectrumData=pd.DataFrame(np.transpose([wavelength,normalized_amplitude]))
 #         spectrumData.to_csv(Path(data_root_directory+sub_folder+save_folder+"spectrum"+str(fileCount)+".csv"),header=None)
 #         spectrum.to_csv(Path(data_root_directory+sub_folder+save_folder+"rawDataFile"+str(fileCount)+".csv"),header=None)
