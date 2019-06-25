b63e95affc53910ed31eeb6bd97facda2b9fb140
sean <sean@Lethe>
2019-06-25 17:27:13 -0500

    Update to spectrum fit and visualize_data

--- SX674_Spectrometer/spectrum_fit.py
+++ SX674_Spectrometer/spectrum_fit.py
@@ -19,8 +19,12 @@ initial_fit=[[601.0,612,625],\
 
 temp_cal=[601.007,0.0078] # from center wavelength vs temp: [intercept, slope]
 
+start=585
+stop=630
+tolerance=0.3
+
 # lmodel.set_param_hint('c1',min=600,max=602)
-lmodel.set_param_hint('c2',min=605,max=620)
+lmodel.set_param_hint('c2',min=605,max=625)
 lmodel.set_param_hint('c3',min=605,max=630)
         
 lmodel.set_param_hint('w1',min=0)
@@ -32,42 +36,51 @@ params=lmodel.make_params(c1=initial_fit[0][0],w1=initial_fit[1][0],h1=initial_f
                           c3=initial_fit[0][2],w3=initial_fit[1][2],h3=initial_fit[2][2],\
                           o1=initial_fit[3])
 
-fit_curves=input_data[0].to_numpy()
 fit_values=[]
 temp_values=[]
 
 file_count=0
 
-
 for i in range(1,len(input_data.columns)):
 
-    result=lmodel.fit(input_data[i],params,x=input_data[0])
+    lambdaStartIndex=input_data[input_data[[0]].apply(np.isclose,b=start,atol=tolerance).any(1)].index.tolist()[0]
+    lambdaStopIndex=input_data[input_data[[0]].apply(np.isclose,b=stop,atol=tolerance).any(1)].index.tolist()[0]
+        
+    wavelength=input_data.iloc[lambdaStartIndex:lambdaStopIndex,0].to_numpy()
+    amplitude=input_data.iloc[lambdaStartIndex:lambdaStopIndex,i].to_numpy()
+    normalized_amplitude=(amplitude-amplitude.min())/(amplitude.max()-amplitude.min())
+
+    if(i==1):
+        fit_curves=wavelength
+
+    result=lmodel.fit(normalized_amplitude,params,x=wavelength)
     print(result.best_values)
 
-    fit_curves=np.c_[fit_curves,result.best_fit.to_numpy()]
+    fit_curves=np.c_[fit_curves,result.best_fit]
     
     fit_values.append(findValue(result.best_values))
     
     temp_value=(findValue(result.best_values)[0]-temp_cal[0])/temp_cal[1]
     temp_values.append(temp_value)
 
-#     plt.plot(input_data[0],input_data[i])
-#     plt.plot(input_data[0],result.best_fit,ls='dashed')
+#     # Comment out this code block to suppress plotting data
+#     plt.plot(wavelength,normalized_amplitude)
+#     plt.plot(wavelength,result.best_fit,ls='dashed')
 #     if(file_count>2):
 #         break
 #     file_count+=1
 # plt.show()
 
+# Comment out this block to suppress data writing to disk
 temp_stdev=np.std(np.transpose(temp_values)[1:len(np.transpose(temp_values))])
-
+  
 fit_curves=pd.DataFrame(fit_curves)
 fit_values=pd.DataFrame(fit_values)
 temp_values=pd.DataFrame(temp_values)
-
+  
 fit_curves.to_csv(Path(Path(data_directory)/(save_directory+"fit_curves.csv")),\
                        index=False,header=None)
 fit_values.to_csv(Path(Path(data_directory)/(save_directory+"fit_values.csv")),\
                        index=False,header=None)
 temp_values.to_csv(Path(Path(data_directory)/(save_directory+"fit_temp_values.csv")),\
                        index=False,header=None)
-
